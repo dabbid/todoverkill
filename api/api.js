b65ba03b5a4e230d1f25f779631166d13f383c8b
Dav <david.daignan@protonmail.com>
2017-12-19 00:44:31 +0100

    Add modification date on update Add API logs

--- api/api.js
+++ api/api.js
@@ -9,6 +9,7 @@ var uid = 4;
 router.get('/todos', function(req, res, next) {
   jsonfile.readFile(dataFilePath, function(err, json) {
     res.send(json);
+    console.log('Todo list requested');
   });
 });
 
@@ -19,6 +20,7 @@ router.get('/todos/:id', function(req, res, next) {
         return todo.id === Number(req.params.id);
       });
       res.send(requestedTodo);
+      console.log(`Todo #${requestedTodo.id} requested`);
     });
   }
 });
@@ -31,6 +33,7 @@ router.post('/todos', function(req, res, next) {
   jsonfile.writeFile(dataFilePath, todo, { flag: 'a' }, function(err) {
     if (!err) {
       res.send(todo);
+      console.log(`New todo created: ${JSON.stringify(todo, null, 2)}`);
     }
   });
 });
@@ -48,11 +51,12 @@ router.put('/todos/:id', function(req, res, next) {
           return false;
         });
         if (requestedTodo) {
-          requestedTodo = Object.assign(requestedTodo, req.body);
+          requestedTodo = Object.assign(requestedTodo, req.body, { modified_at: new Date().toISOString() });
           todos[requestedTodoIndex] = requestedTodo;
           jsonfile.writeFile(dataFilePath, todos, function(err) {
             if (!err) {
               res.send(requestedTodo);
+              console.log(`Todo #${requestedTodo.id} updated:\n${JSON.stringify(requestedTodo, null, 2)}`);
             }
           });
         }
