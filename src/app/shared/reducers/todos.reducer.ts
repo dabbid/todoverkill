aa37c05dcf2f52f251c6140dbb7289ec6447f13a
Dav <david.daignan@protonmail.com>
2017-12-18 14:24:23 +0100

    Update todo in list when it's been updated

--- src/app/shared/reducers/todos.reducer.ts
+++ src/app/shared/reducers/todos.reducer.ts
@@ -1,7 +1,10 @@
 import { TODOS_LOADED, TODOS_LOADING } from '../actions/todos.types';
+import { TODO_UPDATED } from '../actions/todo.types';
 import * as actions from '../actions/todos.actions';
+import { TodoUpdatedAction } from '../actions/todo.actions';
+import Todo from '../models/todo.model';
 
-export type Action = actions.All;
+export type Action = actions.All | TodoUpdatedAction;
 
 const initialState = {
   list: [],
@@ -27,6 +30,20 @@ export default function listReducer(state = initialState, action: Action) {
         loaded: true,
         loading: false,
       };
+
+    case TODO_UPDATED:
+      // update targeted todo in list
+      const list:Array<Todo> = state.list.slice(0);
+      let updatedTodoIndex = -1;
+      let updatedTodo = list.find((todo, index) => {
+        if (todo.id === action.payload.id) {
+          updatedTodoIndex = index;
+          return true;
+        }
+        return false;
+      });
+      list[updatedTodoIndex] = Object.assign({}, updatedTodo, action.payload);
+      return { ...state, list };
 
     default:
       return state;
